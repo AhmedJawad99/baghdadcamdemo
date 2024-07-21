cbfbb9700dd49e3d61a493d59f2e5de821440c2b
Ahmed Jawad <30501645+AhmedJawad99@users.noreply.github.com>
2024-07-22 00:06:35 +0300

    cvrr

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "986720781"
+    serviceWorkerVersion: "3572015777"
   }
 });
